b6c527f892cc8d762421ab2b3d7c64ab8d15b0af
Felipe Facundes <felipe.facundes@gmail.com>
2021-09-12 10:58:49 -0300

    Add files via upload

---
+++ config/hybrid-gpu-nvidia/etc/optimus-manager/nvidia-enable.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script is only executed if you set switching to "custom" in optimus-manager.conf.
+# Everything you write here will be executed by optimus-manager as root prior to loading the nvidia kernel modules.
+# Use this to set your own commands for powering up the Nvidia GPU.
