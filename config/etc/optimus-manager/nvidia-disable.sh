5e8283e342aac1aafb6a65fce6a557a9c53c9188
Felipe Facundes <felipe.facundes@gmail.com>
2021-09-12 11:02:12 -0300

    Add files via upload

---
+++ config/etc/optimus-manager/nvidia-disable.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script is only executed if you set switching to "custom" in optimus-manager.conf.
+# Everything you write here will be executed by optimus-manager as root after unloading the nvidia kernel modules.
+# Use this to set your own commands for powering down the Nvidia GPU.
